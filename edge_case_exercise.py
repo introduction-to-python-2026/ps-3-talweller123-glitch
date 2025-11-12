f7b63e8387bc442f80a1d2fa7bc97fca7cb8bcb0
talweller123-glitch <talweller123@gmail.com>
2025-11-12 23:59:00 +0200

    Update edge_case_exercise.py

--- edge_case_exercise.py
+++ edge_case_exercise.py
@@ -1,15 +1,1 @@
-def move(my_list, direction):
- 
-    idx = my_list.index(1)
 
-    if direction == 'right':
-        if idx < len(my_list) - 1:
-            my_list[idx] = 0
-            my_list[idx + 1] = 1
-
-    elif direction == 'left':
-        if idx > 0:
-            my_list[idx] = 0
-            my_list[idx - 1] = 1
-    
-    return my_list
