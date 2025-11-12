8e523536b3dfa6b89a86fd2e97215402e42dce0f
talweller123-glitch <talweller123@gmail.com>
2025-11-13 00:16:42 +0200

    Update leibniz_series_exercise.py1

--- leibniz_series_exercise.py
+++ leibniz_series_exercise.py
@@ -1,1 +1,13 @@
-
+def approximate_pi(n_terms):
+    list_of_numbers = []
+    x = []
+    for i in range(n_terms):
+        list_of_numbers.append(i)
+    for i in list_of_numbers:
+        x.append (((-1) ** i)/(2*i + 1))
+    pi1 = 0
+    for i in x:
+        pi1 += i
+    pi = 4*pi1
+    print (pi)
+approximate_pi(100)
